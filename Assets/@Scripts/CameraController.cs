8f44be871b48f9c32197143946770e2015a902cf
HDRP <hdrp0720@gmail.com>
2023-12-08 21:19:50 +0900

    add script of camera controller

---
+++ Assets/@Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  [SerializeField] private Transform _followTarget;         // 카메라 주시 대상
+  [SerializeField] private float _distance = 5f;            // 캐릭터-카메라 거리 조절
+  [SerializeField] private float _rotationSpeed = 2f;       // 카메라 회전 속도
+  [SerializeField] private float _minVerticalAngle = -20;   // x축 회전 최소 각도
+  [SerializeField] private float _maxVerticalAngle = 45;    // x축 회전 최대 각도
+
+  [SerializeField] private Vector2 _framingOffset;          // 캐릭터-카메라 높이 조절 (피봇)
+
+  [SerializeField] private bool _invertX;                   // x축 회전 방향 반전
+  [SerializeField] private bool _invertY;                   // y축 회전 방향 반전
+
+  private float _rotationX;
+  private float _rotationY;
+
+  private float _invertXVal;
+  private float _invertYVal;
+
+  private void Start()
+  {
+    Cursor.visible = false;
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+  private void Update()
+  {
+    _invertXVal = (_invertX) ? -1 : 1;
+    _invertYVal = (_invertY) ? -1 : 1;
+    
+    _rotationX += Input.GetAxis("Mouse Y") * _rotationSpeed * _invertXVal;
+    _rotationX = Mathf.Clamp(_rotationX, _minVerticalAngle, _maxVerticalAngle);
+    
+    _rotationY += Input.GetAxis("Mouse X") * _rotationSpeed * _invertYVal;
+
+    var targetRotation = Quaternion.Euler(_rotationX, _rotationY, 0);
+
+    var focusPosition = _followTarget.position + new Vector3(_framingOffset.x, _framingOffset.y);
+    
+    transform.position = focusPosition - targetRotation * new Vector3(0, 0, _distance);
+    transform.rotation = targetRotation;
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ﻿### Version
-* Unity 2022.3.15f1g
+* Unity 2022.3.15f1
+
+### Used Asset
+* Hot Reload
