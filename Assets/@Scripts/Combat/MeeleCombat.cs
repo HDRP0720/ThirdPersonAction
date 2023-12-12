86c59b1725628b62c94f47200a0dff172a22a25b
HDRP <hdrp0720@gmail.com>
2023-12-12 18:01:03 +0900

    add hit reaction when was attacked

--- Assets/@Scripts/Combat/MeeleCombat.cs
+++ Assets/@Scripts/Combat/MeeleCombat.cs
@@ -11,10 +11,20 @@ public class MeeleCombat : MonoBehaviour
   public bool IsInAction { get; private set; } = false;
   
   private static readonly int SS_Slash01 = Animator.StringToHash("SS_Slash01");
+  private static readonly int Hit_Fwd = Animator.StringToHash("Hit_Fwd");
 
   private void Awake()
   {
     _animator = GetComponent<Animator>();
+  }
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.CompareTag("Hitbox") && !IsInAction)
+    {
+      StartCoroutine(PlayHitReaction());
+      Debug.Log($"{this.gameObject.name} was hit!!");
+    
+    }
   }
 
   public void TryToAttack()
@@ -29,6 +39,19 @@ public class MeeleCombat : MonoBehaviour
   {
     IsInAction = true;
     _animator.CrossFade(SS_Slash01, 0.2f);
+    yield return null;
+    
+    var animState = _animator.GetNextAnimatorStateInfo(1);
+    
+    yield return new WaitForSeconds(animState.length);
+
+    IsInAction = false;
+  }
+  
+  private IEnumerator PlayHitReaction()
+  {
+    IsInAction = true;
+    _animator.CrossFade(Hit_Fwd, 0.2f);
     yield return null;
     
     var animState = _animator.GetNextAnimatorStateInfo(1);
--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ### Used Asset
 * [Amplify Animation Pack](https://assetstore.unity.com/packages/3d/animations/amplify-animation-pack-207911)
 * [Hot Reload](https://assetstore.unity.com/packages/tools/utilities/hot-reload-edit-code-without-compiling-254358)
+* [Long Sword](https://assetstore.unity.com/packages/3d/props/weapons/long-sword-212082)
+* [Polygon Dungeon](https://assetstore.unity.com/packages/3d/environments/dungeons/polygon-dungeons-low-poly-3d-art-by-synty-102677)
 * [Unity-Chan! Sunny Side Up(URP)](https://github.com/unity3d-jp/UnityChanSSU/releases/download/1.0.5/UnityChanSSU_URP-release-1.0.5.zip)
   * 전용 셰이더 적용을 위해 `URP-HighFidelity-Renderer`설정의 `Depth Priming Mode`를 `Disabled`로 변경
   * Unity-Chan SSU를 위해서 `Packages/manifest.json` 파일 내에 아래 코드를 작성하여 추가.
